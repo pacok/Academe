6bdca12b2ec9e81b1a82f09d090ba3af054cded6
Francisco Camas <pacocamas85@gmail.com>
2019-06-10 00:44:35 +0200

    eliminacion de archivos

--- Academe-backend/src/main/java/com/academe/apirest/models/dao/CursoDao.java
+++
@@ -1,9 +0,0 @@
-package com.academe.apirest.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.academe.apirest.models.entity.Curso;
-
-public interface CursoDao extends CrudRepository<Curso, Long>{
-
-}
--- Academe-backend/src/main/java/com/academe/apirest/models/dao/UsuarioDao.java
+++
@@ -1,9 +0,0 @@
-package com.academe.apirest.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.academe.apirest.models.entity.Usuario;
-
-public interface UsuarioDao extends CrudRepository<Usuario, Long> {
-
-}
